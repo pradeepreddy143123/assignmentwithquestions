8dca3ccf8e07ef774a4df2a9a215aec1ffa9b213
pradeep <epradeepreddy2001@gmail.com>
2022-09-09 19:29:01 +0530

    6_datatypes_2_Q6

---
+++ c_variables/datatypes/6_datatypes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+float r;
+float a;
+float p;
+printf("enter the radius");
+scanf("%f",&r);
+a=3.14*r*r;
+p=2*3.14*r;
+printf("perimeter of a circle=%f",p);
+printf("area of a circle=%f",a);
+return 0;
+}
