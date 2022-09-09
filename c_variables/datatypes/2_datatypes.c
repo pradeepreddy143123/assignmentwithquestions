90f794fe015034f7770ba469e41f2d473feabed5
pradeep <epradeepreddy2001@gmail.com>
2022-09-09 19:21:36 +0530

    datatypes_2_Q2

---
+++ c_variables/datatypes/2_datatypes.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+int x;
+char c;
+float f;
+unsigned int u;
+short int hi;
+long int ld;
+unsigned short int hu;
+unsigned long int lu;
+signed char sc;
+unsigned char uc;
+double lf;
+
+scanf("%d",&x);
+printf("%d ",x);
+
+scanf(" %c",&c);
+printf("%c ",c);
+
+scanf("%f",&f);
+printf("%f ",f);
+
+scanf("%u",&u);
+printf("%u ",u);
+
+scanf("%hi",&hi);
+printf("%hi ",hi);
+
+scanf("%ld",&ld);
+printf("%ld ",ld);
+
+scanf("%hu",&hu);
+printf("%hu ",hu);
+
+scanf("%lu",&lu);
+printf("%lu ",lu);
+ 
+scanf(" %c",&sc);
+printf("%c ",sc);
+
+scanf(" %c",&uc);
+printf("%c ",uc);
+
+scanf("%lf",&lf);
+printf("%lf ",lf);
+
+return 0;
+}
