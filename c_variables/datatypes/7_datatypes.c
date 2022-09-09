cd4ae0f3bfa057c99d915edc9cff229c6314412d
pradeep <epradeepreddy2001@gmail.com>
2022-09-09 19:30:17 +0530

    7_datatypes_2_Q7

---
+++ c_variables/datatypes/7_datatypes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+float s;
+float a;
+float p;
+printf("enter the sides of square");
+scanf("%f",&s);
+a=s*s;
+p=4*s;
+printf("Area of a square=%f",a);
+printf("perimeter of a square=%f",p);
+return 0;
+}
