991026c7910852f2f79371a03976e1cbfe9c558f
pradeep <epradeepreddy2001@gmail.com>
2022-09-09 19:27:58 +0530

    5_datatypes_2_Q5

---
+++ c_variables/datatypes/5_datatypes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int l,b;
+float p;
+float a;
+printf("enter the length and breadth of rectangle");
+scanf("%d%d",&l,&b);
+p=2*(l+b);
+a=l*b;
+printf("%f",p);
+printf("%f",a);
+return 0;
+}
