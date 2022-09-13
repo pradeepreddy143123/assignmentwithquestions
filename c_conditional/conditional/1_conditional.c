5793fb13b91fbe7744fd6a52b1c48017d447be2b
pradeep <epradeepreddy2001@gmail.com>
2022-09-13 21:05:11 +0530

    1_conditional.c_4_Q1.

---
+++ c_conditional/conditional/1_conditional.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the two numbers");
+scanf("%d%d",&a,&b);
+if (a>b)
+printf("the biggest number is %d",a);
+else  
+printf("the biggest number is %d",b);
+return 0;
+}
