a1a8164be752ce8f26c52d0de665b273c067bd67
pradeep <epradeepreddy2001@gmail.com>
2022-09-13 21:07:46 +0530

    2_conditional.c_4_Q2

---
+++ c_conditional/conditional/2_conditional.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+int a;
+printf("enter a number");
+scanf("%d",&a);
+if (a%2==0)
+{
+printf("it is a even number %d",a);
+}
+else
+printf("it is a odd number %d",a);
+return 0;
+}
