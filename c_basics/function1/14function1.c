73c845232a09e66c8d518bbf9abb43d397936d4d
pradeep <epradeepreddy2001@gmail.com>
2022-09-30 15:15:30 +0530

    whileloop3

---
+++ c_basics/function1/14function1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int square(int a)
+{
+	int i=0,c=0;
+	while(i<a)
+	{
+		c=c+a;
+		i++;
+	}
+	return c;
+}
+int main()
+{
+	int a,c;
+	printf("enter the numbers");
+	scanf("%d",&a);
+	c=square(a);
+	printf("%d\n",c);
+}
+
+
+
