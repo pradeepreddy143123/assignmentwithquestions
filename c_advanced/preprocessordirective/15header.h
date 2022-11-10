5e5ec11b5c9fc901370dd8d040720c3ae8604d9f
pradeep <epradeepreddy2001@gmail.com>
2022-11-10 16:05:51 +0530

    deleted preprocessordirectivefolder

--- c_advanced/preprocessordirective/15header.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-//#include<stdio.h>
-void available_stock();
-void sell();
-void purchase();
-void addstock(int item);
-int deletestock(int item);
-
--- c_advanced/preprocessordirective/3calcheader.h
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-#define add(a,b) (a+b)
-#define sub(a,b) (a-b)
-#define mul(a,b) (a*b)
-#define div(a,b) (a/b)
-
