efd99f07b88cba92d34b0e7569f8911008d328a9
pradeep <epradeepreddy2001@gmail.com>
2022-12-06 19:14:05 +0530

    extraassignment

--- c_advanced/extraassignment/3wordexpansion.c
+++ c_advanced/extraassignment/3wordexpansion.c
@@ -1,31 +1,51 @@
 #include<stdio.h>
+int wordexpansion(char str[])
+{
+int i,b=0;
+for(i=0;str[i]!='\0';i++)
+{
+b=str[i]-48;
+switch(b)
+{
+case 1:
+printf("one ");
+break;
+case 2:
+printf("two ");
+break;
+case 3:
+printf("three ");
+break;
+case 4:
+printf("four ");
+break;
+case 5:
+printf("five ");
+break;
+case 6:
+printf("six ");
+break;
+case 7:
+printf("seven ");
+break;
+case 8:
+printf("eight ");
+break;
+case 9:
+printf("nine ");
+break;
+case 0:
+printf("zero ");
+break;
+}
+}
+
+printf("\n");
+}
 int main()
 {
-	int num[10],i=0,bi=0,ib,c=0;
-	printf("enter a number");
-	for(i=0;i<10;i++)
-	{
-		scanf("%d",&num[i]);
-	}
-
-	for(i=0;i<10;i++)
-	{
-		if(num[i]>bi)
-		{
-			bi=num[i];
-			
-		}
+char str[100],d;
+printf("enter number\n");
+scanf("%s",str);
+d=wordexpansion(str);
 }
-		for(i=0;i<10;i++)
-		{
-			if(bi==num[i])
-				c++;
-		}
-	
-
-
-	printf("biggest=%d\n %d\n",bi,c);
-
-}
-
-
