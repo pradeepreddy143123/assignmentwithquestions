1acdc983127bc55f1b22e728bc5c1f14e48daa54
pradeep <epradeepreddy2001@gmail.com>
2022-12-07 16:05:13 +0530

    dynamicmemoryallocation2ndassignment

--- c_advanced/dynamicmemoryallocation2/1trim.c
+++ c_advanced/dynamicmemoryallocation2/1trim.c
@@ -1,25 +1,28 @@
 #include<stdio.h>
+#include<string.h>
 #include<stdlib.h>
-void trim(char s[])
-{
-int i=0,j;
-for(i=0;s[i]==' ';i++);
-
-for(i=0;s[i];i++)
-{
-  for(j=0;s[j] = s[i];i++,j++);
-}
-for(i=0;s[i];i++);
-for(i;s[i]==' ';i--);
- s[i+1]='\0';
-}
-
 int main()
 {
-char *s=(char *)malloc(100*sizeof(char));
+char *a=(char *)malloc(10*sizeof(char)),c;
+printf("%lu",sizeof(a));
+int i,l,j,k,m;
 printf("enter a string\n");
-scanf("%s",s);
-trim(s);
-printf("%s",s);
+scanf("%[^\n]s",a);
+for(i=0;a[i]!=0;i++);
+for(j=0;a[j]==' ';j++);
+for(k=i-1;a[k]==' ';k--);
+k=k-j;
+for(l=j,m=0;a[l]!='\0';l++,m++)
+{
+c=a[l];
+a[m]=c;
 }
+a[k+1]='\0';
+a=(char *)realloc(a,4*(sizeof(char)));
+printf("%s",a);
+free(a);
+printf("\n");
+printf("%s",a);
 
+return 0;
+}
